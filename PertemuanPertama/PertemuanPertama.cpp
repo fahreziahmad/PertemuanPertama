06b81f01dd496756d69861ae3c6afd2489734768
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-02-27 18:54:26 +0700

    Add project files.

---
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{//begin
+
+    //numeric nJejari, nLuas
+    //display 'Masukan Jejari= '
+    //accept nJejari
+    //compute nLuas = 3.14 * r * r
+    //display  'Luasnya= ' + nLuas
+    
+    
+    float nJejari, nLuas;
+    cout << "Masukan jejari =";
+    cin >> nJejari;
+    nLuas = 3.15 * nJejari * nJejari
+
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
